5b0918368ccfd31e23b84e7e70e813ce7518795f
Benjamin Toh <toh.benjamin123@gmail.com>
2023-05-12 15:55:54 +0800

    initial testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+    const [data, setData] = useState({
+        name: "",
+        date: ""
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://127.0.0.1:5000/");
+                const jsonData = await response.json();
+                setData({
+                    name: jsonData.Name,
+                    date: jsonData.Date
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+
+    }, []);
+
+    console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <h1>Test</h1>
+            <p>{data.name}</p>
+            <p>{data.date}</p>
+        </header>
     </div>
   );
 }
